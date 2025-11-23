b0ba6be7477a7e424c88a0476a3911f63a41628d
rebecca <rebecca@recruitment-software.co.uk>
2025-11-23 15:24:29 +0000

    Installed gunicorn. Updated requirements.txt to contain gunicorn. Updated wsgi.py to expose the app object. Updated Dockerfile to use gunicorn.

--- config.py
+++ config.py
@@ -4,8 +4,7 @@ class Config:
     # Use environment variables if set, otherwise fallback to defaults
     SECRET_KEY = os.getenv('SECRET_KEY', 'devsecret123')
     
-    # If DATABASE_URL is set (for Docker), use it
-    # Otherwise, fallback to a local SQLite file
+    # If DATABASE_URL is set (for Docker), ustherwise, fallback to a local SQLite file
     SQLALCHEMY_DATABASE_URI = os.getenv(
         'DATABASE_URL', 'sqlite:///EclipseSoftwareHelpDesk.db'
     )
--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,3 @@
 from HelpDesk import create_app
 
 app = create_app()
-
-app.run(host="0.0.0.0", port=5000)
