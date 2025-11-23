410dfe4b654732e15b6a9a09106fefd046ddd18d
rebecca <rebecca@recruitment-software.co.uk>
2025-11-23 17:58:13 +0000

    Updated docker-compose.

--- config.py
+++ config.py
@@ -4,7 +4,6 @@ import os
 load_dotenv()
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY')
-    
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SECRET_KEY = os.getenv('SECRET_KEY', 'development-key')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///EclipseSoftwareHelpDesk.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
