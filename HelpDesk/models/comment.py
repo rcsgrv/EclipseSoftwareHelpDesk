de761c05ee0e7d0a67d52242dadf95e491a65a62
rebecca <rebecca@recruitment-software.co.uk>
2025-11-24 10:12:39 +0000

    Updated comment.py to contain created_by rather than author_fullname. Updated ticket_details.html to contain created_by from comment.py.

--- HelpDesk/models/comment.py
+++ HelpDesk/models/comment.py
@@ -7,9 +7,9 @@ class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     comment_text = db.Column(db.Text(), nullable=False)
     date_created = db.Column(db.DateTime(timezone=True), default=func.current_timestamp())
-    author_fullname = db.Column(db.String(100), nullable=False)
-    
-    ticket_id = db.Column(db.Integer, db.ForeignKey('ticket.id', ondelete='SET NULL'), nullable=True)
-    ticket = relationship('Ticket', backref='ticket_comments')
+    created_by = db.Column(db.String(100), nullable=False)
 
     user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='SET NULL'), nullable=True)
+    ticket_id = db.Column(db.Integer, db.ForeignKey('ticket.id', ondelete='SET NULL'), nullable=True)
+    
+    ticket = relationship('Ticket', backref='ticket_comments')
